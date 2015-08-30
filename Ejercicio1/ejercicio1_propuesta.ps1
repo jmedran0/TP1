80f6c3c218367af6e9efac569fbccce5e51b87ca
Bruno Sendras <brunosendras@gmail.com>
2015-08-30 01:40:47 -0300

    Ahora se chequea que el parametro $cantidad sea mayor o igual a cero en ejercicio1_propuesta.ps1.

--- Ejercicio1/ejercicio1_propuesta.ps1
+++ Ejercicio1/ejercicio1_propuesta.ps1
@@ -5,8 +5,7 @@
 # Integrantes: APELLIDOS, nombres, DNI
 #    @integrante1
 #    @integrante2
-# Descripción: Script propuesto por la catedra. Modificado con validaciones y mejoras.
-# El objetivo de este script es listar los procesos en ejecusion.
+# Descripción: 
 
 Param
 (
@@ -17,38 +16,46 @@ Param
     [Parameter(Position = 2, Mandatory = $false)][int] $cantidad = 3
 )
 
-# evaluo si el path proporcionado es un directorio.
-$esdirectorio = Test-Path $pathsalida -IsValid -PathType Container 
+# Validamos que la cantidad de procesos a listar por pantalla no sea negativa.
+if ($cantidad -ge 0) { 
 
-if ($esdirectorio -eq $true) {
+    # evaluamos si el path proporcionado es un directorio.
+    $esdirectorio = Test-Path $pathsalida -IsValid -PathType Container 
 
-    # Ahora evaluo si existe.
-    $existe = Test-Path $pathsalida
+    if ($esdirectorio -eq $true) {
 
-    if ($existe -eq $true) {
-        # Uso el path seleccionado, haciendo que la validacion del path tenga sendtido. 
-        $archivo = $pathsalida + "procesos.txt"
-        
-        $listaproceso = Get-Process
+        # Ahora evaluamos si existe.
+        $existe = Test-Path $pathsalida
 
-        foreach ($proceso in $listaproceso) {
-            $proceso | Format-List -Property Id,Name >> $archivo
-        }
-        
-        # Reasigno la cantidad solicitada, para no mostrar mas Procesos de los que hay.
-        if ($cantidad -gt $listaproceso.Count) {
-            $cantidad = $listaproceso.count
-        }
-                   
-        for ($i = 0; $i -lt $cantidad; $i++) {         
-            Write-Host $listaproceso[$i].Name - $listaproceso[$i].Id
-        }
+        if ($existe -eq $true) {
+            # Usamos el path seleccionado, haciendo que la validacion del path tenga sendtido. 
+            $archivo = $pathsalida + "procesos.txt"
+            
+            $listaproceso = Get-Process
+
+            foreach ($proceso in $listaproceso) {
+                $proceso | Format-List -Property Id,Name >> $archivo
+            }
+            
+            # Reasignamos la cantidad solicitada, para no mostrar mas Procesos de los que hay.
+            if ($cantidad -gt $listaproceso.Count) {
+                $cantidad = $listaproceso.count
+            }
+                       
+            for ($i = 0; $i -lt $cantidad; $i++) {         
+                Write-Host $listaproceso[$i].Name - $listaproceso[$i].Id
+            }
+        } else {
+            Write-Host "El directorio no existe."
+        }   
     } else {
-        Write-Host "El directorio no existe."
-    }   
+        Write-Host "El path no es un directorio."
+    }
 } else {
-    Write-Host "El path no es un directorio."
+    Write-Host "La cantidad ingresada es negativa."
 }
+
+
 
 
 
