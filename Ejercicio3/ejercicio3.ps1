0cc1c06349495f1865a149d29419da74383743d5
Bruno Sendras <brunosendras@gmail.com>
2015-09-01 10:12:21 -0300

    Modificado el script ejercicio3.ps1 para que: 	- Pregunte al usuario si desea sobreescribir el archivo. 	- Respete el nombre del archivo de origen.

--- Ejercicio3/ejercicio3.ps1
+++ Ejercicio3/ejercicio3.ps1
@@ -7,22 +7,71 @@
 #    @integrante2
 # Descripción
 
+<#  
+    .SYNOPSIS 
+      Lee el contenido del archivo de texto -archivo con el en formato:
+        campo1=valor1
+        campo2=valor2
+        campo3=valor3
+        ***
+        campo1=valor4
+        campo2=valor5
+        campo3=valor6
+
+      y lo exporta a un archivo CSV en el directorio -directorioDeSalida proporcionado. 
+       
+    .EXAMPLE
+     .\ejercicio3.ps1 .\backup.txt .\
+
+     Este comando exporta el contenido de backup.txt al archivo backup.csv en el directorio actual.
+#>
+
 Param (
     # Ambos parametros son rutas, por lo que validamos el largo apropiado segun la plataforma .NET
-    [ValidateLength(1, 260)][Parameter(Position = 1, Mandatory = $false)][String]$archivo,
-    [ValidateLength(1, 260)][Parameter(Position = 2, Mandatory = $false)][String]$directorioDeSalida
+    [ValidateLength(1, 260)][Parameter(Position = 1, Mandatory = $true)][String]$archivo,
+    [ValidateLength(1, 260)][Parameter(Position = 2, Mandatory = $true)][String]$directorioDeSalida
 )
 
 # Validamos la existencia del archivo de origen.
 $existeArchivo = Test-Path -path $archivo -PathType Leaf
 if ($existeArchivo -eq $true) {
     
+    # Construimos la ruta completa del archivo de salida
+    # 1 - Nos quedamos con el nombre del archivo de origen.
+    # 2 - Unimos el nombre al path destino seleccionado
+    # 3 - Reemplazamos la extensión.  
+    $NombreDeArchivoDeEntrada = Split-Path $archivo -Leaf
+    $archivoDeSalida = Join-Path $directorioDeSalida $NombreDeArchivoDeEntrada
+    $archivoDeSalida = $archivoDeSalida -replace ".txt", ".csv"          
+
     # Validamos la existencia del archivo de origen.
     $existeDirectorioDeSalida = Test-Path -path $directorioDeSalida -PathType Container
+    
     if ($existeDirectorioDeSalida -eq $true) {
         
+        # Si el archivo existe, preguntamos si debemos reemplazar.
+        if (Test-Path -Path $archivoDeSalida -PathType Leaf) {
+            
+            # Preguntamos hasta que se ingrese una opción correcta.            
+            do {
+                Write-Warning "El archivo destino ya existe. La exportación lo reemplazará."
+                $opcion = Read-Host "Desea Continuar? S/N"
+                $opcion = $opcion.ToUpper()
+                
+                if ($opcion -eq 'S') {
+                    # Borramos y seguimos adelante.
+                    rm -Path $archivoDeSalida -Force 
+                } else {
+                    # Abortamos el proceso.
+                    Write-Host "Exportación cancelada por el usuario."
+                    exit
+                }
+
+            } While ($opcion -ne 'S' -and $opcion -ne 'N')
+        }
+        
         # Leemos el contenido en grupos de 4 lineas, que conform
-        $registros = Get-Content $archivo -ReadCount 4
+        $registros = Get-Content $archivo -ReadCount 4        
         foreach($registro in $registros) {
 
             # Creamos el hash table para almacenar los pares key=Value
@@ -40,13 +89,14 @@ if ($existeArchivo -eq $true) {
                     # Agrego el par ya separado al registro.
                     $htRegistro.Add($nombreCampo, $valor)                          
                 }
+            
             }
-            
-            # Creamos el objeto que puede ser exportado a partir del registro que formamos.
+
+            # A partir del registro que formamos, creamos el objeto que puede ser exportado.
             $obj = New-Object -TypeName PSObject -Property $htRegistro
-            
-            # Exportamos al archivo 'backu.csv' en el directorio seleccionado.
-            $obj | Export-Csv -Path ($directorioDeSalida + "backup.csv") -NoTypeInformation -Force -Append
+
+            # Exportamos al archivo 'backup.csv' en el directorio seleccionado.
+            $obj | Export-Csv -Path ($archivoDeSalida) -NoTypeInformation -Force -Append
         }
        
     } else {
